ffa6e2a9a4d30a440cdf10f6e44490e10acdae79
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-19 11:45:08 -0800

    Created interfaces to represent the payload descriptors and contract generators

---
+++ src/Microsoft.AspNet.SignalR.Core/Payloads/IPayloadDescriptorProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.AspNet.SignalR.Infrastructure
+{
+    public interface IPayloadDescriptorProvider
+    {
+        /// <summary>
+        /// Retrieve all avaiable payload types.
+        /// </summary>
+        /// <returns>Collection of payload descriptors.</returns>
+        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "This call might be expensive")]
+        IEnumerable<PayloadDescriptor> GetPayloads();
+    }
+}

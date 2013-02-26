26876bea8e4f9fa5b5af96fff07fc468ca1f76da
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-25 21:37:14 -0800

    Created ContractedClientHubInvocation that allows for Contract Id's to be passed down to the client in order to decompress data.

---
+++ src/Microsoft.AspNet.SignalR.Core/Compression/ContractedClientHubInvocation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNet.SignalR.Hubs;
+using Newtonsoft.Json;
+
+namespace Microsoft.AspNet.SignalR.Compression
+{
+    public class ContractedClientHubInvocation : ClientHubInvocation
+    {
+        public ContractedClientHubInvocation(ClientHubInvocation invocation)
+        {
+            Target = invocation.Target;
+            Hub = invocation.Hub;
+            Method = invocation.Method;
+            Args = invocation.Args;
+            State = invocation.State;
+        }
+
+        /// <summary>
+        /// A klist of contract Ids, it's a 1-to-1 map to args
+        /// </summary>
+        [JsonProperty("C")]
+        public long[] ContractIds { get; set; }
+    }
+}

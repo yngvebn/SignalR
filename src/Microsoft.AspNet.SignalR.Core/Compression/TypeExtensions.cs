6ab5c38d7490885e75603fbfff8651b4d40fb516
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-27 17:33:20 -0800

    Added TypeExtensions that are used to determine if a type is an ienumerable and what type of ienumerable it is

---
+++ src/Microsoft.AspNet.SignalR.Core/Compression/TypeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.AspNet.SignalR.Compression
+{
+    public static class TypeExtensions
+    {
+        public static Boolean IsEnumerable(this Type type)
+        {
+            return type.GetInterfaces()
+                        .Where(t => t.IsGenericType)
+                        .Where(t => t.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                        .Count() > 0;
+        }
+
+        public static Type GetEnumerableType(this Type type)
+        {
+            Type[] typeList = type.GetGenericArguments();
+
+            if (typeList.Length > 0)
+            {
+                return type.GetGenericArguments()[0];
+            }
+            else if (type.IsArray)
+            {
+                return type.GetElementType();
+            }
+
+            return typeof(object);
+        }
+    }
+}

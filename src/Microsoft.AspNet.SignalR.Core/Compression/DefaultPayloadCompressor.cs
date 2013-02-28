bd46d62eb62f80a89ac7a19dc4ab119b1864155c
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-27 17:37:00 -0800

    Updated DefaultPayloadCompressor to utilize enumerable types

--- src/Microsoft.AspNet.SignalR.Core/Compression/DefaultPayloadCompressor.cs
+++ src/Microsoft.AspNet.SignalR.Core/Compression/DefaultPayloadCompressor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -24,7 +25,8 @@ namespace Microsoft.AspNet.SignalR.Compression
         {
             if (payload != null)
             {
-                var payloadDescriptor = _provider.GetPayload(payload.GetType());
+                var payloadType = payload.GetType();
+                var payloadDescriptor = _provider.GetPayload(payloadType);
 
                 // Only compress the payload if we have a payload descriptor for it
                 if (payloadDescriptor != null)
@@ -34,6 +36,28 @@ namespace Microsoft.AspNet.SignalR.Compression
                                 // Recursively compress the object value until it's at a base type
                                 return Compress(dataDescriptor.GetValue(payload));
                             });
+                }
+                else
+                {
+                    // At this point the payload object isn't directly a payload but may contain a payload
+                    if (_provider.HasPayload(payloadType))
+                    {
+                        payloadType = payloadType.GetEnumerableType();
+                        var itemType = payload.GetType();
+                        payloadDescriptor = _provider.GetPayload(payloadType);
+                        var payloadList = payload as ICollection;
+                        var compressedList = new List<object>();
+
+                        if (payloadDescriptor != null)
+                        {
+                            foreach (var item in payloadList)
+                            {
+                                compressedList.Add(Compress(item));
+                            }
+
+                            return compressedList;
+                        }
+                    }
                 }
             }
 

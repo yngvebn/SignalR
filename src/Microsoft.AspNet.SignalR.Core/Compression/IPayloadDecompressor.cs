35105ec1046c5fa049236051e5682672f483f5f8
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-27 17:36:19 -0800

    Added a payload decompressor to handle incoming requests from the client

---
+++ src/Microsoft.AspNet.SignalR.Core/Compression/IPayloadDecompressor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNet.SignalR.Json;
+
+namespace Microsoft.AspNet.SignalR.Compression
+{
+    public interface IPayloadDecompressor
+    {
+        object Decompress(object payload, Type expected);
+    }
+}
